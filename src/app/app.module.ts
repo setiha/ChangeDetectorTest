26ba3163f6d5f205aea548b2d8eb4f6b5d17d90e
Tihici <tihamer.sebok@gmail.com>
2020-09-30 16:58:10 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarItemComponent } from './navbar-item/navbar-item.component';
+import { HomeComponent } from './home/home.component';
+import { OnePageComponent } from './one-page/one-page.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarItemComponent,
+    HomeComponent,
+    OnePageComponent,
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule
